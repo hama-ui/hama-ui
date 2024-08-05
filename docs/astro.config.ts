e0101bc6585faba3b39c74d6aa4d3860a0d4357f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-05 13:14:44 +0800

    docs: add gettings-started

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
           items: [
             // Each item here is one entry in the navigation menu.
             { label: 'Example Guide', slug: 'guides/example' },
+            { label: 'Getting Started', slug: 'guides/getting-started' },
           ],
           label: 'Guides',
         },
