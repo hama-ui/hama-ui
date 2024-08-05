3e56a69369d101e1012544d5f24e66b62c53033b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-05 13:46:54 +0800

    chore(preset): update options

--- packages/preset/src/index.ts
+++ packages/preset/src/index.ts
@@ -1,6 +1,8 @@
 import { definePreset } from '@unocss/core'
 
-export interface PresetHamaOptions {}
+export interface PresetHamaOptions {
+  prefix?: `${string}-`
+}
 
 export const presetHama = definePreset((_options: PresetHamaOptions = {}) => ({
   name: '@hama-ui/preset',
