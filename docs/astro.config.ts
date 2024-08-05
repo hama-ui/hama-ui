07b74ca578d1dcf5f3bc02e68f708c6efe7058eb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-05 18:36:58 +0800

    docs: add components

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,7 +1,10 @@
 import starlight from '@astrojs/starlight'
+import unoCSS from '@unocss/astro'
 import { defineConfig } from 'astro/config'
 // @ts-expect-error missing types
 import liveCode from 'astro-live-code'
+
+import unoConfig from '../uno.config'
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,6 +17,10 @@ export default defineConfig({
           label: 'Guides',
         },
         {
+          autogenerate: { directory: 'components' },
+          label: 'Components',
+        },
+        {
           autogenerate: { directory: 'reference' },
           label: 'Reference',
         },
@@ -24,5 +31,6 @@ export default defineConfig({
       title: 'Hama UI',
     }),
     liveCode(),
+    unoCSS(unoConfig),
   ],
 })
