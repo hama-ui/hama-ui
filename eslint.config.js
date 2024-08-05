b51e05854d9ba469d68ceb37cc51808ffc95bb18
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-05 11:07:43 +0800

    chore: add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+import antfu from '@antfu/eslint-config'
+import ii from '@importantimport/eslint-config'
+
+export default antfu({
+  astro: true,
+  typescript: true,
+  unocss: true,
+}).append(ii({ functional: false }))
