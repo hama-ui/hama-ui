0f73140c439dc7ae5fc0d80b388ff0f2efe12cb5
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-05 23:15:58 +0800

    chore: update button

--- packages/preset/src/shortcuts/button.tsx
+++ packages/preset/src/shortcuts/button.tsx
@@ -3,6 +3,6 @@ import type { Theme } from '@unocss/preset-mini'
 
 export const button: UserShortcuts<Theme> = [
   {
-    btn: 'py-2 px-4 font-semibold rounded-lg shadow-md',
+    btn: 'bg-primary-3 text-primary-12 py-2 px-4 font-semibold rounded-lg shadow-md',
   },
 ]
