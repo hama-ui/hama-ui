8b7537cc357fab42723118baee2faf74dd3daafd
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-06 22:04:06 +0800

    refactor(preset): use cva

--- packages/preset/src/shortcuts/button.tsx
+++ packages/preset/src/shortcuts/button.tsx
@@ -1,8 +1,47 @@
 import type { UserShortcuts } from '@unocss/core'
 import type { Theme } from '@unocss/preset-mini'
 
+import { cva } from 'cva'
+
+const buttonVariants = cva({
+  base: [
+    'inline-flex',
+    'items-center',
+    'justify-center',
+    'whitespace-nowrap',
+    'rounded-md',
+    'text-sm',
+    'font-medium',
+  ],
+  compoundVariants: [
+    {
+      size: 'md',
+      variant: 'primary',
+    },
+  ],
+  defaultVariants: {
+    size: 'md',
+    variant: 'primary',
+  },
+  variants: {
+    size: {
+      lg: 'h-10 rounded-md px-8',
+      md: 'h-9 px-4 py-2',
+      sm: 'h-8 rounded-md px-3 text-xs',
+    },
+    variant: {
+      primary: [
+        'bg-primary-10',
+        'text-primary-1',
+        'border-transparent',
+        'hover:bg-primary-11',
+      ],
+    },
+  },
+})
+
 export const button: UserShortcuts<Theme> = [
   {
-    btn: 'bg-primary-3 text-primary-12 py-2 px-4 font-semibold rounded-lg shadow-md',
+    btn: buttonVariants(),
   },
 ]
