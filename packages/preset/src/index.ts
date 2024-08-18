350b55068d5f14289697dfb2f98b8aca6d55a572
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-18 14:59:07 +0800

    refactor(preset): remove attributify preset

--- packages/preset/src/index.ts
+++ packages/preset/src/index.ts
@@ -1,5 +1,4 @@
 import { definePreset } from '@unocss/core'
-import { presetAttributify } from '@unocss/preset-attributify'
 import { presetMini } from '@unocss/preset-mini'
 // https://github.com/unocss/unocss/pull/4053
 import presetWebFonts from '@unocss/preset-web-fonts'
@@ -18,7 +17,6 @@ export const presetHama = definePreset((options: PresetHamaOptions = {}) => ({
   preflights,
   presets: [
     presetMini(),
-    presetAttributify(),
     presetWebFonts(),
   ],
   shortcuts,
