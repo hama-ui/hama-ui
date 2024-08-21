8ef210aa0bb03b67926320f7f16f5e174e382b04
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-21 22:35:05 +0800

    fix(eslint): fix config types

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,10 @@
-import antfu from '@antfu/eslint-config'
+import type { Linter } from 'eslint'
+
+import { antfu } from '@antfu/eslint-config'
 import ii from '@importantimport/eslint-config'
 
 export default antfu({
   astro: true,
   typescript: true,
   unocss: true,
-}).append(ii({ functional: false }))
+}).append(ii({ functional: false })) as Linter.Config
