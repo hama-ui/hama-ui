25bed1c5cf2bd40466cc331a5014d28c0b5f3a7c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-21 23:50:47 +0800

    fix(recipes): set unbuild alias

--- packages/recipes/build.config.ts
+++ packages/recipes/build.config.ts
@@ -1,9 +1,13 @@
+import { fileURLToPath } from 'node:url'
 import { defineBuildConfig } from 'unbuild'
 
 import buildConfig from '../../build.config'
 
 export default defineBuildConfig({
   ...buildConfig[0],
+  alias: {
+    '~': fileURLToPath(new URL('src', import.meta.url)),
+  },
   // entries: [
   //   './src/index',
   //   {
