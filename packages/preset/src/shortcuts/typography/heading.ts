557b0890b235a9cdcc2e84006de297162864c329
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-24 19:33:30 +0800

    fix(preset): add prefix

--- packages/preset/src/shortcuts/typography/heading.ts
+++ packages/preset/src/shortcuts/typography/heading.ts
@@ -3,10 +3,10 @@ import type { StaticShortcutMap } from '@unocss/core'
 import { Heading } from '@hama-ui/recipes'
 
 export const heading: StaticShortcutMap = {
-  h1: Heading.variants({ size: 1 }),
-  h2: Heading.variants({ size: 2 }),
-  h3: Heading.variants({ size: 3 }),
-  h4: Heading.variants({ size: 4 }),
-  h5: Heading.variants({ size: 5 }),
-  h6: Heading.variants({ size: 6 }),
+  'prose-h1': Heading.variants({ size: 1 }),
+  'prose-h2': Heading.variants({ size: 2 }),
+  'prose-h3': Heading.variants({ size: 3 }),
+  'prose-h4': Heading.variants({ size: 4 }),
+  'prose-h5': Heading.variants({ size: 5 }),
+  'prose-h6': Heading.variants({ size: 6 }),
 }
